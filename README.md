6d263a423aad3f7046a7a608329fe59d427d6106
pritamkhose <pritamkhose@gmail.com>
2023-10-20 07:45:29 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-i18n
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/pritamkhose/react-i18n)
---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './style.css';
+import './locales/index';
+import { useTranslation } from 'react-i18next';
+
+export default function App() {
+  const { t } = useTranslation();
+
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+      <p>{t('dummyNamespace.medium')}</p>
+      <p>
+        https://levelup.gitconnected.com/complete-i18n-guide-to-support-multi-language-for-your-react-native-app-c5ea4e0fa5b3
+      </p>
+
+      <p>
+        https://github.com/i18next/react-i18next/blob/master/example/react_native_windows/App.js
+      </p>
+
+      <p>https://react.i18next.com/latest/using-with-hooks</p>
+    </div>
+  );
+}
